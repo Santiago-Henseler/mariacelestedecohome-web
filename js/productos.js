f9c191e249120617f573123e2a7bd243d792ea2a
Santy <shenseler@fi.uba.ar>
2024-11-26 16:48:37 -0300

    a

--- js/productos.js
+++ js/productos.js
@@ -1,21 +1,21 @@
 const AROMAS = {
     0:2,
-    1: ["Home spray", 5000, "400mm", "Realizado con perfumeria fina para aromatizar tus textiles y generos mas delicados.", ["imagenes/aromas/spray1.jpg", "imagenes/aromas/spray2.jpg", "imagenes/aromas/spray3.jpg",  "imagenes/aromas/spray4.jpg"]],
-    2: ["Home diffuser", 5000,"300mm", "Difusor de larga duracion para aromatizar tus ambientes. Incluye 4 varillas negras", ["imagenes/aromas/difusor1.jpg", "imagenes/aromas/difusor2.jpg", "imagenes/aromas/difusor3.jpg",  "imagenes/aromas/difusor4.jpg"]],
+    1: ["Home spray", 5000, "400mm", "Realizado con perfumeria fina para aromatizar tus textiles y generos mas delicados.", ["imagenes/aromas/spray1.jpg", "imagenes/aromas/spray2.jpg", "imagenes/aromas/spray3.jpg",  "imagenes/aromas/spray4.jpg"], []],
+    2: ["Home diffuser", 5000,"300mm", "Difusor de larga duracion para aromatizar tus ambientes. Incluye 4 varillas negras", ["imagenes/aromas/difusor1.jpg", "imagenes/aromas/difusor2.jpg", "imagenes/aromas/difusor3.jpg",  "imagenes/aromas/difusor4.jpg"], []],
 }
 
 const BATH = {
     0:3,
-    1: ["Hand cream", 5000,"300mm", "Perfumada y suave para generar una hidratacion de absorcion rapida.", ["imagenes/difusor1.jpg"]],
-    2: ["Hand soap", 5000,"300mm", "Jabon aromático, genera una espuma suave, hipoalergenico", ["imagenes/difusor1.jpg"]],
-    3: ["Body splash", 5000, "", "Especial para llevar en tu cartera, darte frescura durante el dia con nuestro aroma Berna. Un mimo para el stress diario.", ["imagenes/difusor1.jpg"]],
+    1: ["Hand cream", 5000,"300mm", "Perfumada y suave para generar una hidratacion de absorcion rapida.", ["imagenes/difusor1.jpg"], []],
+    2: ["Hand soap", 5000,"300mm", "Jabon aromático, genera una espuma suave, hipoalergenico", ["imagenes/difusor1.jpg"], []],
+    3: ["Body splash", 5000, "", "Especial para llevar en tu cartera, darte frescura durante el dia con nuestro aroma Berna. Un mimo para el stress diario.", ["imagenes/difusor1.jpg"], []],
 }
 
 const TEXTILES = {
     0:3,
-    1: ["Almohadon tusor liso", 5000, "50cm x 50cm", "Descripcion del producto", ["imagenes/almohadon/almohadon3.jpg"]],
-    2: ["Almohadon tusor love", 5000,"30cm x 60cm", "Descripcion del producto", ["imagenes/almohadon/almohadon1.jpg", "imagenes/almohadon/love2.jpg",  "imagenes/almohadon/love3.jpg"]],
-    3: ["Almohadon tusor mandala", 5000, "50cm x 50cm", "Descripcion del producto", ["imagenes/almohadon/almohadon2.jpg",  "imagenes/almohadon/mandala2.jpg",  "imagenes/almohadon/mandala3.jpg"]],
+    1: ["Almohadon tusor liso", 5000, "50cm x 50cm", "Descripcion del producto", ["imagenes/almohadon/almohadon3.jpg"], ["verde militar", "avellana", "arena", "crudo", "khaki"]],
+    2: ["Almohadon tusor love", 5000,"30cm x 60cm", "Bordado artesanal a mano en distintos tipos de lana sobre tela tusor.", ["imagenes/almohadon/almohadon1.jpg", "imagenes/almohadon/love2.jpg",  "imagenes/almohadon/love3.jpg"], ["Blanco con bordado beige", "Crudo con bordado beige", "Crudo con bordado marron", "Khaki con bordado crudo", "Avellana con bordado crudo"]],
+    3: ["Almohadon tusor mandala", 5000, "50cm x 50cm", "Bordado artesanal a mano en distintos tipos de lana sobre tela tusor.", ["imagenes/almohadon/almohadon2.jpg",  "imagenes/almohadon/mandala2.jpg",  "imagenes/almohadon/mandala3.jpg"], ["Arena con bordado beige", "Crudo con bordado beige", "Khaki con bordado crudo"]],
 }
 
 const GIFT = {
