64e5ecd16854d2aeceeeaf7de4fbf483243c47e3
Santy <shenseler@fi.uba.ar>
2024-12-12 17:55:22 -0300

    a

--- js/productos.js
+++ js/productos.js
@@ -6,14 +6,14 @@ const AROMAS = {
 
 const BATH = {
     0:3,
-    1: ["Hand cream", 15000,"300mm", "Perfumada y suave para generar una hidratacion de absorcion rapida.", ["imagenes/difusor1.jpg"], []],
-    2: ["Hand soap", 13000,"300mm", "Jabon aromático, genera una espuma suave, hipoalergenico", ["imagenes/difusor1.jpg"], []],
-    3: ["Body splash", 16000, "", "Especial para llevar en tu cartera, darte frescura durante el dia con nuestro aroma Berna. Un mimo para el stress diario.", ["imagenes/difusor1.jpg"], []],
+    1: ["Hand cream", 15000,"300mm", "Perfumada y suave para generar una hidratacion de absorcion rapida.", ["imagenes/jabon/cream1.jpg", "imagenes/jabon/cream2.jpg"], []],
+    2: ["Hand soap", 13000,"300mm", "Jabon aromático, genera una espuma suave, hipoalergenico", ["imagenes/jabon/soap1.jpg", "imagenes/jabon/soap2.jpg"], []],
+    3: ["Body splash", 16000, "", "Especial para llevar en tu cartera, darte frescura durante el dia con nuestro aroma Berna. Un mimo para el stress diario.", ["imagenes/jabon/body1.jpg", "imagenes/jabon/body2.jpg"], []],
 }
 
 const TEXTILES = {
     0:3,
-    1: ["Almohadon tusor liso", 22000, "50cm x 50cm", "Descripcion del producto", ["imagenes/almohadon/almohadon3.jpg"], ["verde militar", "avellana", "arena", "crudo", "khaki"]],
+    1: ["Almohadon tusor liso", 22000, "50cm x 50cm", "", ["imagenes/almohadon/liso1.jpg", "imagenes/almohadon/liso2.jpg", "imagenes/almohadon/liso3.jpg", "imagenes/almohadon/liso4.jpg", "imagenes/almohadon/liso5.jpg"], ["verde militar", "avellana", "arena", "crudo", "khaki"]],
     2: ["Almohadon tusor love", 25000,"30cm x 60cm", "Bordado artesanal a mano en distintos tipos de lana sobre tela tusor.", ["imagenes/almohadon/almohadon1.jpg", "imagenes/almohadon/love2.jpg",  "imagenes/almohadon/love3.jpg"], ["Blanco con bordado beige", "Crudo con bordado beige", "Crudo con bordado marron", "Khaki con bordado crudo", "Avellana con bordado crudo"]],
     3: ["Almohadon tusor mandala", 26000, "50cm x 50cm", "Bordado artesanal a mano en distintos tipos de lana sobre tela tusor.", ["imagenes/almohadon/almohadon2.jpg",  "imagenes/almohadon/mandala2.jpg",  "imagenes/almohadon/mandala3.jpg"], ["Arena con bordado beige", "Crudo con bordado beige", "Khaki con bordado crudo"]],
 }
