02902831d4449c428c923ab79be69b5ebf5fab19
mariacelestedecohome <celesteventura20@gmail.com>
2025-05-30 09:34:04 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -1,27 +1,27 @@
 const AROMAS = {
     0:2,
-    1: ["Home spray", 17000, "400mm", "Realizado con perfumeria fina para aromatizar tus textiles y generos mas delicados.", ["imagenes/aromas/spray1.jpg", "imagenes/aromas/spray2.jpg", "imagenes/aromas/spray3.jpg",  "imagenes/aromas/spray4.jpg"], ["Oslo", "Verbena", "White Flowers", "Orange Pepper", "Berna"]],
-    2: ["Home diffuser", 18000,"300mm", "Difusor de larga duracion para aromatizar tus ambientes. Incluye 4 varillas negras", ["imagenes/aromas/difusor1.jpg", "imagenes/aromas/difusor2.jpg", "imagenes/aromas/difusor3.jpg"], ["Oslo", "Verbena", "White Flowers", "Orange Pepper", "Berna"]],
+    1: ["Home spray", 18000, "400mm", "Realizado con perfumeria fina para aromatizar tus textiles y generos mas delicados.", ["imagenes/aromas/spray1.jpg", "imagenes/aromas/spray2.jpg", "imagenes/aromas/spray3.jpg",  "imagenes/aromas/spray4.jpg"], ["Oslo", "Verbena", "White Flowers", "Orange Pepper", "Berna"]],
+    2: ["Home diffuser", 20000,"300mm", "Difusor de larga duracion para aromatizar tus ambientes. Incluye 4 varillas negras", ["imagenes/aromas/difusor1.jpg", "imagenes/aromas/difusor2.jpg", "imagenes/aromas/difusor3.jpg"], ["Oslo", "Verbena", "White Flowers", "Orange Pepper", "Berna"]],
 }
 
 const BATH = {
     0:3,
-    1: ["Hand cream", 15000,"300mm", "Perfumada y suave para generar una hidratacion de absorcion rapida.", ["imagenes/jabon/cream1.jpg", "imagenes/jabon/cream2.jpg"], []],
-    2: ["Hand soap", 13000,"300mm", "Jabon aromático, genera una espuma suave, hipoalergenico", ["imagenes/jabon/soap1.jpg", "imagenes/jabon/soap2.jpg"], []],
-    3: ["Body splash", 16000, "100mm", "Especial para llevar en tu cartera y darte frescura durante el dia con nuestro aroma Berna. Un mimo para el stress diario.", ["imagenes/jabon/body1.jpg", "imagenes/jabon/body2.jpg"], []],
+    1: ["Hand cream", 13000,"300mm", "Perfumada y suave para generar una hidratacion de absorcion rapida.", ["imagenes/jabon/cream1.jpg", "imagenes/jabon/cream2.jpg"], []],
+    2: ["Hand soap", 12000,"300mm", "Jabon aromático, genera una espuma suave, hipoalergenico", ["imagenes/jabon/soap1.jpg", "imagenes/jabon/soap2.jpg"], []],
+    3: ["Body splash", 15000, "100mm", "Especial para llevar en tu cartera y darte frescura durante el dia con nuestro aroma Berna. Un mimo para el stress diario.", ["imagenes/jabon/body1.jpg", "imagenes/jabon/body2.jpg"], []],
 }
 
 const TEXTILES = {
     0:3,
-    1: ["Almohadones lisos tusor", 22000, "50cm x 50cm", "Hechos en tela tusor. Incluyen relleno de vellón siliconado y cierre metálico oculto. Con funda extraible.", ["imagenes/almohadon/liso1.jpg", "imagenes/almohadon/liso2.jpg", "imagenes/almohadon/liso3.jpg", "imagenes/almohadon/liso4.jpg", "imagenes/almohadon/liso5.jpg"], ["verde militar", "avellana", "arena", "crudo", "khaki"]],
-    2: ["Almohadones love tusor", 25000,"30cm x 60cm", "Bordado artesanal a mano en distintos tipos de lana sobre tela tusor. Incluyen relleno de vellón siliconado y cierre metálico oculto. Con funda extraible.", ["imagenes/almohadon/almohadon1.jpg", "imagenes/almohadon/love2.jpg",  "imagenes/almohadon/love3.jpg", "imagenes/almohadon/love4.jpg"], ["Blanco con bordado beige", "Crudo con bordado beige", "Crudo con bordado marron", "Khaki con bordado crudo", "Avellana con bordado crudo"]],
-    3: ["Almohadon mandala tusor", 26000, "50cm x 50cm", "Bordado artesanal a mano en distintos tipos de lana sobre tela tusor. Incluyen relleno de vellón siliconado y cierre metálico oculto. Con funda extraible.", ["imagenes/almohadon/mandala1.jpg",  "imagenes/almohadon/mandala2.jpg",  "imagenes/almohadon/mandala3.jpg"], ["Arena con bordado beige", "Crudo con bordado beige", "Khaki con bordado crudo"]],
+    1: ["Almohadones lisos tusor", 24000, "50cm x 50cm", "Hechos en tela tusor. Incluyen relleno de vellón siliconado y cierre metálico oculto. Con funda extraible.", ["imagenes/almohadon/liso1.jpg", "imagenes/almohadon/liso2.jpg", "imagenes/almohadon/liso3.jpg", "imagenes/almohadon/liso4.jpg", "imagenes/almohadon/liso5.jpg"], ["verde militar", "avellana", "arena", "crudo", "khaki"]],
+    2: ["Almohadones love tusor", 24000,"30cm x 60cm", "Bordado artesanal a mano en distintos tipos de lana sobre tela tusor. Incluyen relleno de vellón siliconado y cierre metálico oculto. Con funda extraible.", ["imagenes/almohadon/almohadon1.jpg", "imagenes/almohadon/love2.jpg",  "imagenes/almohadon/love3.jpg", "imagenes/almohadon/love4.jpg"], ["Blanco con bordado beige", "Crudo con bordado beige", "Crudo con bordado marron", "Khaki con bordado crudo", "Avellana con bordado crudo"]],
+    3: ["Almohadon mandala tusor", 30000, "50cm x 50cm", "Bordado artesanal a mano en distintos tipos de lana sobre tela tusor. Incluyen relleno de vellón siliconado y cierre metálico oculto. Con funda extraible.", ["imagenes/almohadon/mandala1.jpg",  "imagenes/almohadon/mandala2.jpg",  "imagenes/almohadon/mandala3.jpg"], ["Arena con bordado beige", "Crudo con bordado beige", "Khaki con bordado crudo"]],
 }
 
 const DECO = {
     0:2,
-    1: ["Bandeja marmolada chica", 10000, "10cm x 18cm", "Bandejas simil marmol de resina", ["imagenes/deco/tabla1.jpg", "imagenes/deco/tabla2.jpg", "imagenes/deco/tabla3.jpg"], ["Blanca", "Negra"]],
-    2: ["Bandeja marmolada maxi", 16000, "24cm x 12cm", "Bandejas simil marmol de resina", ["imagenes/deco/tabla2.jpg", "imagenes/deco/tabla1.jpg", "imagenes/deco/tabla3.jpg"], ["Blanca", "Negra"]],
+    1: ["Bandeja marmolada chica", 14000, "10cm x 18cm", "Bandejas simil marmol de resina", ["imagenes/deco/tabla1.jpg", "imagenes/deco/tabla2.jpg", "imagenes/deco/tabla3.jpg"], ["Blanca", "Negra"]],
+    2: ["Bandeja marmolada maxi", 18000, "24cm x 12cm", "Bandejas simil marmol de resina", ["imagenes/deco/tabla2.jpg", "imagenes/deco/tabla1.jpg", "imagenes/deco/tabla3.jpg"], ["Blanca", "Negra"]],
 }
 
 const GIFT = {
